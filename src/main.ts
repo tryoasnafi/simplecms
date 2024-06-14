33badfbb9c733370fe2d95a96b7dd48e87c3c379
Tryo Asnafi <tryoasnafi@gmail.com>
2024-06-15 01:21:18 +0700

    fix: use port from env

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const port = process.env.API_PORT || 3000;
+  const logger = new Logger('bootstrap');
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(port);
+
+  logger.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();

4f0a8ce8216504c2ce168cb28338fd32a3b61d55
Tryo Asnafi <tryoasnafi@gmail.com>
2024-06-16 12:05:24 +0700

    add mongodb module

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { PrismaService } from './prisma/prisma.service';
 
 @Module({
+  imports: [MongooseModule.forRoot(process.env.MONGO_DB_CONN_STRING)],
   providers: [PrismaService],
 })
 export class DatabaseModule {}
